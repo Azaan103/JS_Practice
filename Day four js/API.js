92b03fc787df4b63a947886e770a85547207bbe5
Azaan <iamazaanrahim@gmail.com>
2026-06-03 12:43:10 +0500

    Api code push

--- Day four js/API.js
+++ Day four js/API.js
@@ -1,10 +1,13 @@
-async function getdata() {
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+let joke = document.querySelector('.joke')
 
+async function getdata() {
+    let response = await fetch('https://official-joke-api.appspot.com/random_joke')
     let data = await response.json()
 
+    joke.textContent=data.setup + "" + data.punchline
     console.log(data)
-
+    
+        
     return 200
 }
 
